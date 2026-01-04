6e186c15c4dbd8ba6e4d0eb28c70051294b3449b
takisea <rinriner84@gmail.com>
2026-01-04 22:23:17 +0900

    サイト全体にrevalidate設定する

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -1,9 +1,12 @@
 import Hero from '@/app/_components/Hero';
 import Sheet from '@/app/_components/Sheet';
 
+
 type Props = {
   children: React.ReactNode;
 };
+
+export const revalidate = 60;
 
 export default function NewsLayout({ children }: Props) {
   return (
